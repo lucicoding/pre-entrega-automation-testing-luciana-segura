442d4272075dc9ffac4f3ca1228c162bb0847c81
Lu <lucilus10@gmail.com>
2026-05-11 15:21:40 -0300

    feat: agrego funcion nombre del primer producto y la uso en variable

--- tests/test_carrito.py
+++ tests/test_carrito.py
@@ -6,10 +6,11 @@ def test_agregar_producto_carrito():
     page= SauceDemoPage(driver)
     page.abrir_pagina()
     page.hacer_login("standard_user")
+    nombre_producto= page.obtener_nombre_primer_producto()
     page.agregar_primer_producto()
     assert page.obtener_contador_carrito()=="1"
     page.abrir_carrito()
     assert page.producto_en_carrito()
-    assert page.nombre_producto_carrito()=="Sauce Labs Backpack"
+    assert page.nombre_producto_carrito()==nombre_producto
     driver.quit()
     
